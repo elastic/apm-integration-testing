e98cd2c28f9565cfb85718bedb3b712c49fa1147
simitt <silvia.mitter@elastic.co>
2017-11-27 10:52:40 +0100

    WIP - Fix version handling.

--- scripts/ci/versions_nodejs.sh
+++ scripts/ci/versions_nodejs.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+if [ $# -lt 2 ]; then
+  echo "Argument missing, nodejs_agent_version and apm_server_version must be provided"
+  exit 2
+fi
+
+export NODEJS_AGENT_VERSION=$1
+export APM_SERVER_VERSION=$2
+
 export AGENTS="nodejs"
 export TEST_CMD="pytest tests/agent/test_nodejs.py -v -m version"
 
--- scripts/ci/versions_python.sh
+++ scripts/ci/versions_python.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+if [ $# -lt 2 ]; then
+  echo "Argument missing, python_agent_version and apm_server_version must be provided"
+  exit 2
+fi
+
+export PYTHON_AGENT_VERSION=$1
+export APM_SERVER_VERSION=$2
+
 export AGENTS="python"
 export TEST_CMD="pytest tests/agent/test_python.py -v -m version"
 
