56ce9609e0c5abc4c2e07137a952d051f087eef4
simitt <silvia.mitter@elastic.co>
2017-11-27 11:31:29 +0100

    Remove docker containers.

--- docker/prepare_docker.sh
+++ docker/prepare_docker.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+set -x
+
 containers=$(docker ps -a -q)
 if [[ ! -z ${containers} ]]; then
   docker stop ${containers}
+  docker rm ${containers}
 fi
 docker volume prune -f
 
-set -ex
+set -e
 
 if [[ ! -z ${NETWORK} ]]; then
   nw_ct=$(docker network ls -q --filter="name=${NETWORK}" | wc -l)
