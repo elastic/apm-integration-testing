d2047a283366c850a7b1f20369537b3020ce861a
Juan A <juan@opbeat.com>
2017-12-05 10:11:43 +0100

    Add `server_url` env var.
    Python agent renamed `server` config option to `server_url`, it is easier to set both env vars in the tests than figuring out which one to choose depending on the version

--- docker/python/django/testapp/testapp/settings.py
+++ docker/python/django/testapp/testapp/settings.py
@@ -44,6 +44,7 @@ ELASTIC_APM = {
     'APP_NAME': os.environ['DJANGO_APP_NAME'],
     'SERVER_URL': os.environ['APM_SERVER_URL'],
     'SERVER': os.environ['APM_SERVER_URL'],
+    'SERVER_URL': os.environ['APM_SERVER_URL'],
     'SECRET_TOKEN': '1234_test',
     'TRACES_SEND_FREQ': 1,
     'MAX_EVENT_QUEUE_LENGTH': 1,
