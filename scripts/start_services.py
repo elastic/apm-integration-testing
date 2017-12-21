3d9e9f62cba5548bedd67437c23ffb75bcc46839
Gil Raphaelli <gil@elastic.co>
2017-12-21 14:46:21 -0500

    bump es & kibana to latest release

--- scripts/start_services.py
+++ scripts/start_services.py
@@ -13,7 +13,7 @@ def elasticsearch():
         os.environ['ES_NAME'] = 'elasticsearch'
         os.environ['ES_URL'] = "http://elasticsearch:{}".format(
             os.environ['ES_PORT'])
-        set_version('ES_VERSION', "6.0.0", "release")
+        set_version('ES_VERSION', "6.1.1", "release")
         start("docker/elasticsearch/start.sh")
     else:
         parsed_url = urlparse(os.environ['ES_URL'])
@@ -28,7 +28,7 @@ def kibana():
         os.environ['KIBANA_HOST'] = 'kibana'
         os.environ['KIBANA_URL'] = "http://kibana:{}".format(
             os.environ['KIBANA_PORT'])
-        set_version('KIBANA_VERSION', "6.0.0", "release")
+        set_version('KIBANA_VERSION', "6.1.1", "release")
         start("docker/kibana/start.sh")
     return os.environ['KIBANA_URL']
 
