84b4653943c77b9711cb4c4d82d5e6e2a9d99fa0
Gil Raphaelli <gil@elastic.co>
2017-12-29 17:59:35 -0500

    let docker run do the env var labor

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -10,17 +10,17 @@ docker run \
   --name=${name} \
   --network=${NETWORK} \
   --security-opt seccomp=unconfined \
-  -e ES_URL=${ES_URL} \
-  -e KIBANA_URL=${KIBANA_URL} \
-  -e APM_SERVER_URL=${APM_SERVER_URL} \
-  -e EXPRESS_APP_NAME=${EXPRESS_APP_NAME} \
-  -e EXPRESS_URL=${EXPRESS_URL} \
-  -e FLASK_SERVICE_NAME=${FLASK_SERVICE_NAME} \
-  -e FLASK_URL=${FLASK_URL} \
-  -e DJANGO_SERVICE_NAME=${DJANGO_SERVICE_NAME} \
-  -e DJANGO_URL=${DJANGO_URL} \
+  -e ES_URL \
+  -e KIBANA_URL \
+  -e APM_SERVER_URL \
+  -e EXPRESS_APP_NAME \
+  -e EXPRESS_URL \
+  -e FLASK_SERVICE_NAME \
+  -e FLASK_URL \
+  -e DJANGO_SERVICE_NAME \
+  -e DJANGO_URL \
+  -e URLS \
   -e PYTHONDONTWRITEBYTECODE=1 \
-  -e URLS=${URLS} \
   -e TEST_CMD="${TEST_CMD}" \
   --rm "${name}" \
   /bin/bash \
