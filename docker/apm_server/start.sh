10a365440c927ca72d73a2eb8181bb6418f6dbe9
Silvia Mitter <silvia.mitter@elastic.co>
2018-01-11 15:28:20 +0100

    Use unreleased 6.2 snapshots for ES, Kibana by default (#49)

--- docker/apm_server/start.sh
+++ docker/apm_server/start.sh
@@ -20,7 +20,7 @@ fi
 
 echo "APM_SERVER_VERSION: ${APM_SERVER_VERSION}, ${APM_SERVER_VERSION_STATE}"
 
-if [[ ${APM_SERVER_VERSION_STATE} != "release" ]];then
+if [[ ${APM_SERVER_VERSION_STATE} == "github" ]];then
   docker build -f 'docker/apm_server/Dockerfile' -t ${APM_SERVER_NAME}:${APM_SERVER_VERSION} .
   docker run -d \
     --name="${APM_SERVER_NAME}" \
@@ -36,6 +36,20 @@ if [[ ${APM_SERVER_VERSION_STATE} != "release" ]];then
         make update apm-server
         ./apm-server -e -d \"*\" -c ../apm-server.yml"
 else
+  image_name="docker.elastic.co/apm/apm-server:${APM_SERVER_VERSION}"
+
+  if [[ ${APM_SERVER_VERSION_STATE} == "snapshot" ]];then
+    registry="https://snapshots.elastic.co/docker/"
+    name="apm-server"
+    version="${APM_SERVER_VERSION}-SNAPSHOT"
+    file_type="tar.gz"
+    file="${name}-${version}.${file_type}"
+    wget "${registry}${file}"
+    docker load -i "${file}"
+    rm "${file}"
+    image_name="docker.elastic.co/apm/${name}:${version}"
+  fi
+
   docker run -d \
     --name="${APM_SERVER_NAME}" \
     --network="${NETWORK}" \
@@ -43,5 +57,5 @@ else
     -e "ES_HOST=${ES_HOST}" \
     -e "ES_PORT=${ES_PORT}" \
     -v "$(pwd)/docker/apm_server/apm-server.yml:/usr/share/apm-server/apm-server.yml" \
-    --rm "docker.elastic.co/apm/apm-server:${APM_SERVER_VERSION}"
+    --rm "${image_name}"
 fi
--- scripts/start_services.py
+++ scripts/start_services.py
@@ -13,7 +13,7 @@ def elasticsearch():
         os.environ['ES_NAME'] = 'elasticsearch'
         os.environ['ES_URL'] = "http://elasticsearch:{}".format(
             os.environ['ES_PORT'])
-        set_version('ES_VERSION', "6.1.1", "release")
+        set_version('ES_VERSION', "6.2.0", "snapshot")
         start("docker/elasticsearch/start.sh")
     else:
         parsed_url = urlparse(os.environ['ES_URL'])
@@ -28,7 +28,7 @@ def kibana():
         os.environ['KIBANA_HOST'] = 'kibana'
         os.environ['KIBANA_URL'] = "http://kibana:{}".format(
             os.environ['KIBANA_PORT'])
-        set_version('KIBANA_VERSION', "6.1.1", "release")
+        set_version('KIBANA_VERSION', "6.2.0", "snapshot")
         start("docker/kibana/start.sh")
     return os.environ['KIBANA_URL']
 
