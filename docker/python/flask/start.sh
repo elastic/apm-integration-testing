3893d0dde501d0c44feef1b35296d974cc11587a
Benjamin Wohlwend <bw@piquadrat.ch>
2018-05-15 09:28:02 -0400

    added 2.0 release of Python agent to integration tests (#63)
    * added 2.0 release and "latest" of Python agent to integration tests
    * Bugfix installing released python agent version.

--- docker/python/flask/start.sh
+++ docker/python/flask/start.sh
@@ -3,8 +3,10 @@
 set -ex
 
 if [[ -z ${PYTHON_AGENT_VERSION} ]] || 
-   [[ -z ${PYTHON_AGENT_VERSION_STATE} ]]; then
-  echo "PYTHON_AGENT_VERSION, PYTHON_AGENT_VERSION_STATE expected."
+   [[ -z ${PYTHON_AGENT_VERSION_STATE} ]] ||
+   [[ -z ${FLASK_SERVICE_NAME} ]] ||
+   [[ -z ${FLASK_PORT} ]]; then
+  echo "PYTHON_AGENT_VERSION, PYTHON_AGENT_VERSION_STATE, FLASK_SERVICE_NAME and FLASK_PORT expected."
   exit 2
 fi
 
@@ -20,26 +22,24 @@ fi
 echo "PYTHON_AGENT_VERSION: ${PYTHON_AGENT_VERSION}, ${PYTHON_AGENT_VERSION_STATE}"
 
 if [[ ${PYTHON_AGENT_VERSION_STATE} == "release" ]]; then
-  install_cmd="elastic-apm==${PYTHON_AGENT_VERSION}"
+  if [[ ${PYTHON_AGENT_VERSION} == "latest" ]]; then
+    install_cmd="elastic-apm"
+  else
+    install_cmd="elastic-apm==${PYTHON_AGENT_VERSION}"
+  fi
 else
   install_cmd="git+https://github.com/elastic/apm-agent-python.git@${PYTHON_AGENT_VERSION}"
 fi
 
-app_name=${FLASK_SERVICE_NAME}
-port=${FLASK_PORT}
-start_cmd="python app.py"
-
-docker build --pull -t ${app_name} -f ./docker/python/flask/Dockerfile .
-
+docker build --pull -t ${FLASK_SERVICE_NAME} -f ./docker/python/flask/Dockerfile .
 
 docker run -d \
-  --name ${app_name} \
+  --name ${FLASK_SERVICE_NAME} \
   --network=${NETWORK} \
-  -p ${port}:${port} \
-  -e FLASK_SERVICE_NAME=${app_name} \
-  -e FLASK_PORT=${port} \
+  -p ${FLASK_PORT}:${FLASK_PORT} \
+  -e FLASK_SERVICE_NAME=${FLASK_SERVICE_NAME} \
+  -e FLASK_PORT=${FLASK_PORT} \
   -e APM_SERVER_URL=${APM_SERVER_URL} \
-  --rm "${app_name}" \
+  --rm "${FLASK_SERVICE_NAME}" \
   /bin/bash \
-  -c "pip install -U ${install_cmd}
-     ${start_cmd}"
+  -c "pip install -U ${install_cmd} && python app.py"
