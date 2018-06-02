a954bee35c9fd6f063f539d3e25bd98e9461b17a
Gil Raphaelli <g@raphaelli.com>
2018-06-02 17:11:49 +0000

    kill any running containers before starting under jenkins

--- scripts/ci/common.sh
+++ scripts/ci/common.sh
@@ -13,3 +13,9 @@ function runTests() {
   export VENV=${VENV:-${TMPDIR:-/tmp/}venv-$$}
   make ${targets}
 }
+
+# assume we're under CI if BUILD_NUMBER is set
+if [ -n "${BUILD_NUMBER}" ]; then
+  # kill any running containers under CI
+  docker ps -aq | xargs -t docker rm -f || true
+fi
