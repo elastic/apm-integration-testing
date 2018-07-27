eb95152a22d3022f013320937da41530f28506fd
Benjamin Wohlwend <beni@elastic.co>
2018-07-27 09:50:48 +0200

    use opbeans/opbeans-java base image

--- docker/opbeans/java/entrypoint.sh
+++ docker/opbeans/java/entrypoint.sh
@@ -2,11 +2,11 @@
 if [[ -f /local-install/pom.xml ]]; then
     echo "Using Java agent from from local folder"
     rm -f /app/elastic-apm-agent.jar
-    #Extract current version without using maven which is not avalable in this image
+    #Extract current version without using maven which is not available in this image
     JAVA_AGENT_LOCAL_VERSION=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' /local-install/pom.xml)
     
     cp -v /local-install/elastic-apm-agent/target/elastic-apm-agent-${JAVA_AGENT_LOCAL_VERSION}.jar /app/elastic-apm-agent.jar
-    # copy to folder inside container to ensure were not poluting the local folder
+    # copy to folder inside container to ensure were not polluting the local folder
     cp -r /local-install ~
     cd ~/local-install && python setup.py install
     cd -
@@ -17,12 +17,5 @@ elif [[ $JAVA_AGENT_VERSION ]]; then
 else
     echo "Using Java agent from the docker image"
 fi
-
-java -javaagent:/app/elastic-apm-agent.jar -Dspring.profiles.active=customdb\
-                                        -Dserver.port=${OPBEANS_SERVER_PORT:-3002}\
-                                        -Dspring.datasource.url=${DATABASE_URL:-jdbc:postgresql://postgres/opbeans?user=postgres&password=verysecure}\
-                                        -Dspring.datasource.driverClassName=${DATABASE_DRIVER:-org.postgresql.Driver}\
-                                        -Dspring.jpa.database=${DATABASE_DIALECT:-POSTGRESQL}\
-                                        -jar /app/app.jar
 
 exec "$@"
