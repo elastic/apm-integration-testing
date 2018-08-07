304576d2edf48c40c4d936b50a118885595bc409
Hamid <h.jahtalab@gmail.com>
2018-08-07 11:49:15 +0200

    RUM JS test use utils.check_request_response

--- tests/agent/test_rum.py
+++ tests/agent/test_rum.py
@@ -6,8 +6,8 @@ import requests
 def test_rum(rum):
     elasticsearch = rum.apm_server.elasticsearch
     elasticsearch.clean()
-    endpoint = Endpoint(rum.url, "run_integration_test")
+    endpoint = Endpoint(rum.url, "run_integration_test", qu_str="echo=done", text="done")
 
     r = requests.get(endpoint.url)
-    assert r.status_code == 200
+    utils.check_request_response(r, endpoint)
     utils.check_elasticsearch_transaction(elasticsearch, 1, {'query': {'term': {'processor.event': 'transaction'}}})
