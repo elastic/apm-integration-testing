a8e4328262cf5196b20ceb22dc0474e3951455a0
Gil Raphaelli <g@raphaelli.com>
2018-09-11 17:45:11 -0400

    disable dashboard loading in integration tests

--- scripts/ci/go.sh
+++ scripts/ci/go.sh
@@ -4,6 +4,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 . ${srcdir}/common.sh
 
-DEFAULT_COMPOSE_ARGS="master --no-kibana --with-agent-go-net-http --force-build --build-parallel"
+DEFAULT_COMPOSE_ARGS="master --no-apm-server-dashboards --no-kibana --with-agent-go-net-http --force-build --build-parallel"
 export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
 runTests env-agent-go docker-test-agent-go
--- scripts/ci/nodejs.sh
+++ scripts/ci/nodejs.sh
@@ -4,6 +4,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 . ${srcdir}/common.sh
 
-DEFAULT_COMPOSE_ARGS="master --no-kibana --with-agent-nodejs-express --force-build --build-parallel"
+DEFAULT_COMPOSE_ARGS="master --no-apm-server-dashboards --no-kibana --with-agent-nodejs-express --force-build --build-parallel"
 export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
 runTests env-agent-nodejs docker-test-agent-nodejs
