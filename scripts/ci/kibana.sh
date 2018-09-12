ff239bc04ab8bb77144a7d0054cd034267e1fa68
Gil Raphaelli <g@raphaelli.com>
2018-09-11 20:59:29 -0400

    disable dashboard loading in integration tests (#177)

--- scripts/ci/kibana.sh
+++ scripts/ci/kibana.sh
@@ -4,6 +4,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 . ${srcdir}/common.sh
 
-DEFAULT_COMPOSE_ARGS="master"
+DEFAULT_COMPOSE_ARGS="master --no-apm-server-dashboards"
 export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
 runTests env-kibana docker-test-kibana
