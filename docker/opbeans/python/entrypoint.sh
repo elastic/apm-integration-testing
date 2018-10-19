56608e1065a28765ec1c1459d616b903d9c22f11
Benjamin Wohlwend <beni@elastic.co>
2018-10-19 13:52:10 +0200

    fix entrypoint scripts to work in cases where only branch is defined

--- docker/opbeans/python/entrypoint.sh
+++ docker/opbeans/python/entrypoint.sh
@@ -9,7 +9,7 @@ if [[ -f /local-install/setup.py ]]; then
 elif [[ $PYTHON_AGENT_VERSION ]]; then
     pip install -U elastic-apm==$PYTHON_AGENT_VERSION
 elif [[ $PYTHON_AGENT_BRANCH ]]; then
-    if [ -z ${PYTHON_AGENT_REPO+x} ]; then
+    if [[ -z ${PYTHON_AGENT_REPO} ]]; then
         PYTHON_AGENT_REPO="elastic/apm-agent-python"
     fi
     pip install -U https://github.com/${PYTHON_AGENT_REPO}/archive/${PYTHON_AGENT_BRANCH}.zip
