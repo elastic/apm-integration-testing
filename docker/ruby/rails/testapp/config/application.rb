f9ff1c637cb770eaddc90321a37cafbc90cec1d7
Gil Raphaelli <g@raphaelli.com>
2018-11-21 15:05:44 -0500

    filter healthcheck monitoring with ignore pattern

--- docker/ruby/rails/testapp/config/application.rb
+++ docker/ruby/rails/testapp/config/application.rb
@@ -27,5 +27,8 @@ module Minimal
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Elastic APM config
+    config.elastic_apm.ignore_url_patterns = '/healthcheck'
   end
 end
