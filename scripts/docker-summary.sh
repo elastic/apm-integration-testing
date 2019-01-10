55bd5a6673df44f87edd9156b7d0a28ef3afd5ea
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-01-10 18:51:22 +0100

    show docker inspect only on broken containers

--- scripts/docker-summary.sh
+++ scripts/docker-summary.sh
@@ -2,15 +2,15 @@
 set -exuo pipefail
 
 echo "***************Docker Containers Summary***************"
-docker ps -a --no-trunc
+docker ps -a
 
 DOCKER_IDS=$(docker ps -aq)
 
 for id in ${DOCKER_IDS}
 do
   echo "***************Docker Container ${id}***************"
-  docker ps -af id=${id}
+  docker ps -af id=${id} --no-trunc
   docker logs ${id}
-  docker inspect ${id}
+  docker ps -aq -f id=${id} -f status=dead -f status=exited -f health=unhealthy|xargs -n1 -t docker inspect
 done
 echo "*******************************************************"
