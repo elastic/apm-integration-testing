c82c8bc85ae6efdb0a2c0aaff9654f5d0dcf1b7e
Gil Raphaelli <g@raphaelli.com>
2019-01-29 21:39:45 -0500

    always rebuild container images

--- scripts/ci/7.0-upgrade.sh
+++ scripts/ci/7.0-upgrade.sh
@@ -4,7 +4,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 . ${srcdir}/common.sh
 
-export COMPOSE_ARGS="6.6 --no-apm-server-dashboards --no-apm-server-self-instrument --apm-server-count 2 --apm-server-tee --elasticsearch-data-dir '' --no-apm-server-pipeline --all --no-kibana"
+export COMPOSE_ARGS="6.6 --force-build --build-parallel --no-apm-server-dashboards --no-apm-server-self-instrument --apm-server-count 2 --apm-server-tee --elasticsearch-data-dir '' --no-apm-server-pipeline --all --no-kibana"
 make start-env docker-compose-wait
 
 # let opbeans apps generate some data
