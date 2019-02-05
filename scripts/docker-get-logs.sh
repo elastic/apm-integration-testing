957016a15b62d0d72c0133bb2a2a7f306d0437fb
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-02-05 17:27:23 +0100

    [APM-CI] Get docker logs 6.x (#309)
    * [APM-CI] get docker logs and containers info at the end of tests
    
    * editorconfig for pipeline files

---
+++ scripts/docker-get-logs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+STEP=${1:-""}
+
+mkdir -p docker-info${STEP}
+cd docker-info${STEP}
+
+docker ps -a &> docker-containers.txt
+
+DOCKER_IDS=$(docker ps -aq)
+
+for id in ${DOCKER_IDS}
+do
+  docker ps -af id=${id} --no-trunc &> ${id}-cmd.txt
+  docker logs ${id} &> ${id}.log
+  docker inspect ${id} &> ${id}-inspect.json
+done
