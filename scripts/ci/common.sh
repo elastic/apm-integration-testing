d87941dfd7f3e9ea7e9fe57f86b95c7c4babd74e
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-02-15 20:46:46 +0100

    apply --release

--- scripts/ci/common.sh
+++ scripts/ci/common.sh
@@ -21,9 +21,9 @@ if [ -n "${APM_SERVER_BRANCH}" ]; then
  APM_SERVER_BRANCH_TYPE=${APM_SERVER_BRANCH//$APM_SERVER_BRANCH_VERSION/}
  APM_SERVER_BRANCH_TYPE=${APM_SERVER_BRANCH_TYPE//;/}
  if [ "${APM_SERVER_BRANCH_TYPE}" != "--release" ]; then
-  BUILD_OPTS="${BUILD_OPTS} --apm-server-build https://github.com/elastic/apm-server.git@${APM_SERVER_BRANCH_VERSION} ${APM_SERVER_BRANCH_TYPE} "
+  BUILD_OPTS="${BUILD_OPTS} --apm-server-build https://github.com/elastic/apm-server.git@${APM_SERVER_BRANCH_VERSION}"
  else
-   ELASTIC_STACK_VERSION=${APM_SERVER_BRANCH_VERSION}
+   ELASTIC_STACK_VERSION="${APM_SERVER_BRANCH_VERSION} --release"
  fi
 fi
 
@@ -38,3 +38,8 @@ if [ -n "${BUILD_NUMBER}" ]; then
   # kill any running containers under CI
   [ -n "$(docker ps -aq)" ] && docker ps -aq | xargs -t docker rm -f || true
 fi
+
+echo "ELASTIC_STACK_VERSION=${ELASTIC_STACK_VERSION}"
+echo "APM_SERVER_BRANCH_VERSION=${APM_SERVER_BRANCH_VERSION}"
+echo "APM_SERVER_BRANCH_TYPE=${APM_SERVER_BRANCH_TYPE}"
+echo "BUILD_OPTS=${BUILD_OPTS}"
