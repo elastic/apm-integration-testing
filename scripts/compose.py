15ec9954e240e444f06df694d0d3e5d89d1ba3a5
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2019-03-07 17:08:41 +0100

    pass server url as build args

--- scripts/compose.py
+++ scripts/compose.py
@@ -895,7 +895,7 @@ class Zookeeper(Service):
 class AgentRUMJS(Service):
     SERVICE_PORT = 8000
     DEFAULT_AGENT_BRANCH = "master"
-    DEFAULT_AGENT_REPO = "elastic/apm-agent-js-base"
+    DEFAULT_AGENT_REPO = "elastic/apm-agent-rum-js"
 
     def __init__(self, **options):
         super(AgentRUMJS, self).__init__(**options)
@@ -922,6 +922,7 @@ class AgentRUMJS(Service):
                 args=[
                     "RUM_AGENT_BRANCH=" + self.agent_branch,
                     "RUM_AGENT_REPO=" + self.agent_repo,
+                    "APM_SERVER_URL=" + self.options.get("apm_server_url", DEFAULT_APM_SERVER_URL)
                 ]
             ),
             container_name="rum",
@@ -930,7 +931,7 @@ class AgentRUMJS(Service):
             logging=None,
             environment={
                 "ELASTIC_APM_SERVICE_NAME": "rum",
-                "APM_SERVER_URL": self.options.get("apm_server_url", DEFAULT_APM_SERVER_URL),
+                "ELASTIC_APM_SERVER_URL": self.options.get("apm_server_url", DEFAULT_APM_SERVER_URL)
             },
             ports=[self.publish_port(self.port, self.SERVICE_PORT)],
         )
