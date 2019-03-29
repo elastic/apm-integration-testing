9fe0f1a088c408faf8433e0a1ef72257513118b8
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-03-29 13:57:39 +0100

    Add example for AllowUnknownProperties

--- scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs/scripts/helpers.ts
+++ scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs/scripts/helpers.ts
@@ -1,4 +1,29 @@
-// Allow unknown properties in an object
+// AllowUnknownProperties: Allow unknown properties in an object
+// Example: `type UserWithUnknown = AllowUnknownProperties<User>;`
+//
+// Converts:
+//
+//    interface User {
+//      name: string;
+//      age: number;
+//      address: {
+//        street: string;
+//        city: string;
+//      };
+//    }
+//
+// To this:
+//
+//    type UserWithUnknown = {
+//      name: string;
+//      age: number;
+//      [key: string]: unknown;
+//      address: {
+//        street: string;
+//        city: string;
+//        [key: string]: unknown;
+//      };
+//    };
 export type AllowUnknownProperties<T> = T extends object
   ? { [P in keyof T]: AllowUnknownProperties<T[P]> } & {
       [key: string]: unknown;
