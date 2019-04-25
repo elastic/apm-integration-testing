eeba96badeccd6f8be1ab722746a79329c6efb35
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-04-25 10:40:25 +0100

    Support release or build from source code
    Move the maven package dependencies or generation to the shell script
    
    Add build argument to the compose to support release version for the java agent
    
    Add build argument to the test method
    
    Support release versions for the java agent in the CI
    
    Simplify maven goals to just package and install in the .m2 cache folder
    
    Use the right agent jar file and ensure it is donwloaded when using a release version

--- docker/java/spring/maven-package.sh
+++ docker/java/spring/maven-package.sh
@@ -2,18 +2,19 @@
 set -x
 JAVA_AGENT_BUILT_VERSION=${1}
 
+ARTIFACT_ID=elastic-apm-agent
+
 if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
   cd /agent/apm-agent-java
-  mvn -q --batch-mode package -DskipTests \
+  mvn -q --batch-mode install -DskipTests \
       -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
   export JAVA_AGENT_BUILT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-  mv elastic-apm-agent/target/elastic-apm-agent-${JAVA_AGENT_BUILT_VERSION}.jar /agent/apm-agent.jar
-  mvn -q --batch-mode install:install-file \
-      -Dfile=apm-agent-api/target/apm-agent-api-${JAVA_AGENT_BUILT_VERSION}.jar \
-      -DgroupId=co.elastic.apm -DartifactId=apm-agent-api -Dversion=${JAVA_AGENT_BUILT_VERSION} \
-      -Dpackaging=jar \
-      -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+else
+  mvn -q --batch-mode org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
+      -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+      -DrepoUrl=http://repo1.maven.apache.org/maven2 \
+      -Dartifact=co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}
 fi
 
 cd /app
@@ -21,4 +22,4 @@ mvn -q --batch-mode -DAGENT_API_VERSION=${JAVA_AGENT_BUILT_VERSION} \
   -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
   package
 
-cp /root/.m2/repository/co/elastic/apm/apm-agent-api/${JAVA_AGENT_BUILT_VERSION}/apm-agent-api-${JAVA_AGENT_BUILT_VERSION}.jar /agent/apm-agent.jar
+cp /root/.m2/repository/co/elastic/apm/${ARTIFACT_ID}/${JAVA_AGENT_BUILT_VERSION}/${ARTIFACT_ID}-${JAVA_AGENT_BUILT_VERSION}.jar /agent/apm-agent.jar
