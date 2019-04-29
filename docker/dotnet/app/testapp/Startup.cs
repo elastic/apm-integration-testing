795feaa6a9df356a00355a207edc3c7c75adfa62
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-04-29 11:02:10 +0100

    [APM-CI][Integration Tests] Support test app dotnet

---
+++ docker/dotnet/app/testapp/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Elastic.Apm.All;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestAppDotnet
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration) => Configuration = configuration;
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseElasticApm(Configuration);
+
+			app.UseDeveloperExceptionPage();
+
+			app.UseHttpsRedirection();
+			app.UseMvc();
+		}
+	}
+}
