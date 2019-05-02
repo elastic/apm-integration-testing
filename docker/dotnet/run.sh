12d716a4cac2488397a54b27431dbdab42ce894c
Victor Martinez <victormartinezrubio@gmail.com>
2019-05-02 17:02:06 +0100

    [APM-CI][.NET] Support build from source code and avoid fetching from nuget (#1)

--- docker/dotnet/run.sh
+++ docker/dotnet/run.sh
@@ -5,11 +5,14 @@ CSPROJ="TestAspNetCoreApp.csproj"
 if [ -z "${DOTNET_AGENT_VERSION}" ] ; then
   git clone https://github.com/${DOTNET_AGENT_REPO}.git /src/dotnet-agent -b ${DOTNET_AGENT_BRANCH}
   cd /src/dotnet-agent
+  ### Otherwise: /usr/share/dotnet/sdk/2.2.203/NuGet.targets(119,5): error : The local source '/src/local-packages' doesn't exist. [/src/dotnet-agent/ElasticApmAgent.sln]
+  mkdir /src/local-packages
   dotnet restore
-  dotnet publish -c Release -o build
+  dotnet pack -c Release -o /src/local-packages
 
   cd /src/aspnetcore
-  sed -ibck 's#<PropertyGroup>#<PropertyGroup><RestoreSources>$(RestoreSources);/src/dotnet-agent/src/Elastic.Apm.All/build;https://api.nuget.org/v3/index.json</RestoreSources>#' ${CSPROJ}
+  mv ../NuGet.Config .
+  sed -ibck 's#<PropertyGroup>#<PropertyGroup><RestoreSources>$(RestoreSources);/src/local-packages;https://api.nuget.org/v3/index.json</RestoreSources>#' ${CSPROJ}
   DOTNET_AGENT_VERSION=$(cat /src/dotnet-agent/src/Elastic.Apm.All/Elastic.Apm.All.csproj | grep 'PackageVersion' | sed 's#<.*>\(.*\)<.*>#\1#' | tr -d " ")
   dotnet add package Elastic.Apm.All -v ${DOTNET_AGENT_VERSION}
 fi
--- docker/opbeans/dotnet/run.sh
+++ docker/opbeans/dotnet/run.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 set -x
 git clone https://github.com/${OPBEANS_DOTNET_REPO}.git /src/opbeans-dotnet -b ${OPBEANS_DOTNET_BRANCH}
+CSPROJ="opbeans-dotnet.csproj"
 
 if [ -z "${DOTNET_AGENT_VERSION}" ] ; then
   git clone https://github.com/${DOTNET_AGENT_REPO}.git /src/dotnet-agent -b ${DOTNET_AGENT_BRANCH}
   cd /src/dotnet-agent
+  ### Otherwise: /usr/share/dotnet/sdk/2.2.203/NuGet.targets(119,5): error : The local source '/src/local-packages' doesn't exist. [/src/dotnet-agent/ElasticApmAgent.sln]
+  mkdir /src/local-packages
   dotnet restore
-  dotnet publish -c Release -o build
+  dotnet pack -c Release -o /src/local-packages
 
   cd /src/opbeans-dotnet/opbeans-dotnet
-  sed -ibck 's#<PropertyGroup>#<PropertyGroup><RestoreSources>$(RestoreSources);/src/dotnet-agent/src/Elastic.Apm.All/build;https://api.nuget.org/v3/index.json</RestoreSources>#' opbeans-dotnet.csproj
+  mv ../NuGet.Config .
+  sed -ibck 's#<PropertyGroup>#<PropertyGroup><RestoreSources>$(RestoreSources);/src/local-packages;https://api.nuget.org/v3/index.json</RestoreSources>#' ${CSPROJ}
   DOTNET_AGENT_VERSION=$(cat /src/dotnet-agent/src/Elastic.Apm.All/Elastic.Apm.All.csproj | grep 'PackageVersion' | sed 's#<.*>\(.*\)<.*>#\1#' | tr -d " ")
   dotnet add package Elastic.Apm.All -v ${DOTNET_AGENT_VERSION}
 fi
 
 cd /src/opbeans-dotnet/opbeans-dotnet
 # This is the way to manipulate the csproj with the version of the dotnet agent to be used
-sed -ibck "s#\(<PackageReference Include=\"Elastic\.Apm\.All\" Version=\)\"\(.*\)\"#\1\"${DOTNET_AGENT_VERSION}\"#" opbeans-dotnet.csproj
+sed -ibck "s#\(<PackageReference Include=\"Elastic\.Apm\.All\" Version=\)\"\(.*\)\"#\1\"${DOTNET_AGENT_VERSION}\"#" ${CSPROJ}
 dotnet restore
 dotnet publish -c Release -o build
