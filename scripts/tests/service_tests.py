0e7af28430fc0e10bd20b26446ff74aeb84eb89d
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-05-24 15:30:02 +0200

    ci(jenkins): add support to run Packetbeat to grab network traffic info during integration test (#432)

--- scripts/tests/service_tests.py
+++ scripts/tests/service_tests.py
@@ -8,7 +8,7 @@ from ..compose import (AgentDotnet, AgentGoNetHttp, AgentJavaSpring, AgentNodejs
                        AgentPythonDjango, AgentPythonFlask, AgentRubyRails)
 
 from ..compose import (ApmServer, Kibana, Elasticsearch, Filebeat, Metricbeat,
-                       Logstash, Kafka)
+                       Packetbeat, Logstash, Kafka)
 
 from ..compose import Zookeeper
 
@@ -752,6 +752,36 @@ class MetricbeatServiceTest(ServiceTest):
         ]
         for o in options:
             self.assertTrue(o in beat["command"], "{} not set in {} while output=logstash".format(o, beat["command"]))
+
+
+class PacketbeatServiceTest(ServiceTest):
+    def test_packetbeat(self):
+        packetbeat = Packetbeat(version="6.2.4", release=True).render()
+        self.assertEqual(
+            packetbeat, yaml.load("""
+                packetbeat:
+                    image: docker.elastic.co/beats/packetbeat:6.2.4
+                    container_name: localtesting_6.2.4_packetbeat
+                    user: root
+                    command: packetbeat -e --strict.perms=false -E packetbeat.interfaces.device=eth0 -E setup.dashboards.enabled=true
+                    environment: {}
+                    logging:
+                        driver: 'json-file'
+                        options:
+                            max-size: '2m'
+                            max-file: '5'
+                    depends_on:
+                        elasticsearch:
+                            condition: service_healthy
+                        kibana:
+                            condition: service_healthy
+                    volumes:
+                        - ./docker/packetbeat/packetbeat.yml:/usr/share/packetbeat/packetbeat.yml
+                        - /var/run/docker.sock:/var/run/docker.sock
+                    network_mode: 'service:apm-server'
+                    privileged: 'true'
+                    cap_add: ['NET_ADMIN', 'NET_RAW']""") # noqa: 501
+        )
 
 
 class ZookeeperServiceTest(ServiceTest):
