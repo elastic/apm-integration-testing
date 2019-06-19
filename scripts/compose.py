6e6610fbabba103d79eb54ed0be95072989ce84a
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-06-19 13:35:50 +0200

    fix: allow to set --no-kibana with the --all option (#476)
    * fix: allow to set --no-kibana with the --all option
    
    * Update scripts/compose.py

--- scripts/compose.py
+++ scripts/compose.py
@@ -996,7 +996,7 @@ class Heartbeat(BeatMixin, StackService, Service):
     docker_path = "beats"
 
     def __init__(self, **options):
-        del options['enable_kibana']
+        options['enable_kibana'] = False
         super(Heartbeat, self).__init__(**options)
         config = "heartbeat.yml"
         self.heartbeat_config_path = os.path.join(".", "docker", "heartbeat", config)
