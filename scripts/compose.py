14a02dde1f127af274143bf31f7dc000304d02d7
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-06-28 12:50:15 +0100

    8.0.0 script for the upgrade

--- scripts/compose.py
+++ scripts/compose.py
@@ -699,13 +699,13 @@ class ApmServer(StackService, Service):
                 # always build 7.0
                 backend["build"] = {
                     "args": {
-                        "apm_server_base_image": "docker.elastic.co/apm/apm-server:7.0.0-SNAPSHOT",
+                        "apm_server_base_image": "docker.elastic.co/apm/apm-server:8.0.0-SNAPSHOT",
                         "apm_server_branch": "7.0",
                         "apm_server_repo": "https://github.com/elastic/apm-server.git"
                     },
                     "context": "docker/apm-server"
                 }
-                backend["labels"] = ["co.elatic.apm.stack-version=7.0.0"]
+                backend["labels"] = ["co.elatic.apm.stack-version=8.0.0"]
                 del(backend["image"])  # use the built one instead
             ren.update({"-".join([self.name(), str(i)]): backend})
 
