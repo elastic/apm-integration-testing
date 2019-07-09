a96e9b013bc5dad73df28be68712408ad359d18a
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-09 09:52:02 +0100

    renaming the Elastic.Apm.All package for the opbeans dotnet usage

--- docker/opbeans/dotnet/run.sh
+++ docker/opbeans/dotnet/run.sh
@@ -3,6 +3,9 @@ set -x
 git clone https://github.com/${OPBEANS_DOTNET_REPO}.git /src/opbeans-dotnet -b ${OPBEANS_DOTNET_BRANCH}
 CSPROJ="opbeans-dotnet.csproj"
 
+PACKAGE=Elastic.Apm.NetCoreAll
+CSPROJ_VERSION="/src/dotnet-agent/src/Elastic.Apm.NetCoreAll/${PACKAGE}.csproj"
+
 if [ -z "${DOTNET_AGENT_VERSION}" ] ; then
   git clone https://github.com/${DOTNET_AGENT_REPO}.git /src/dotnet-agent -b ${DOTNET_AGENT_BRANCH}
   cd /src/dotnet-agent
@@ -14,8 +17,8 @@ if [ -z "${DOTNET_AGENT_VERSION}" ] ; then
   cd /src/opbeans-dotnet/opbeans-dotnet
   mv /src/NuGet.Config .
   sed -ibck 's#<PropertyGroup>#<PropertyGroup><RestoreSources>$(RestoreSources);/src/local-packages;https://api.nuget.org/v3/index.json</RestoreSources>#' ${CSPROJ}
-  DOTNET_AGENT_VERSION=$(cat /src/dotnet-agent/src/Elastic.Apm.All/Elastic.Apm.All.csproj | grep 'PackageVersion' | sed 's#<.*>\(.*\)<.*>#\1#' | tr -d " ")
-  dotnet add package Elastic.Apm.All -v ${DOTNET_AGENT_VERSION}
+  DOTNET_AGENT_VERSION=$(cat ${CSPROJ_VERSION} | grep 'PackageVersion' | sed 's#<.*>\(.*\)<.*>#\1#' | tr -d " ")
+  dotnet add package ${PACKAGE} -v ${DOTNET_AGENT_VERSION}
 else
   ### Otherwise: The default NuGet.Config will fail as it's required
   mkdir /src/local-packages
@@ -23,6 +26,6 @@ fi
 
 cd /src/opbeans-dotnet/opbeans-dotnet
 # This is the way to manipulate the csproj with the version of the dotnet agent to be used
-sed -ibck "s#\(<PackageReference Include=\"Elastic\.Apm\.All\" Version=\)\"\(.*\)\"#\1\"${DOTNET_AGENT_VERSION}\"#" ${CSPROJ}
+sed -ibck "s#\(<PackageReference Include=\"Elastic\.Apm\.NetCoreAll\" Version=\)\"\(.*\)\"#\1\"${DOTNET_AGENT_VERSION}\"#" ${CSPROJ}
 dotnet restore
 dotnet publish -c Release -o build
