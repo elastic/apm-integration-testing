12cb07090f79d387c7c86d8ea7a3037897a0293b
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-07-10 09:56:52 +0200

    fix: unspected U+00A0 character (#530)

--- scripts/docker-get-logs.sh
+++ scripts/docker-get-logs.sh
@@ -15,6 +15,6 @@ DOCKER_IDS=$(docker ps -aq)
 for id in ${DOCKER_IDS}
 do
   docker ps -af id=${id} --no-trunc &> ${id}-cmd.txt
-  docker logs ${id} &> ${id}.log || echo "It is not possible to grab the logs of ${id}"
+  docker logs ${id} &> ${id}.log || echo "It is not possible to grab the logs of ${id}"
   docker inspect ${id} &> ${id}-inspect.json || echo "It is not possible to grab the inspect of ${id}"
 done
