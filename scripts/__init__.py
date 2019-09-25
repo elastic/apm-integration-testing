e9da684162a353135078f23478866690a4518c05
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-09-25 11:03:18 +0200

    fix: remove leftovers

--- scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-#from . import modules
-#from . import tests
-
-#from .modules.service import Service, DEFAULT_APM_SERVER_URL
-#from .modules.opbeans import OpbeansService
-#from .modules import BeatMixin, OpbeansRum
