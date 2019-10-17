9507c44c62d71f21926962d4f957d779a29be59f
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-10-17 15:02:13 +0100

    lighter docker image for the obpeans-java

--- docker/opbeans/java/entrypoint.sh
+++ docker/opbeans/java/entrypoint.sh
@@ -1,19 +1,22 @@
-#!/bin/bash -e
-if [[ -f /local-install/pom.xml ]]; then
+#!/usr/bin/env sh
+set -ex
+if [ -f /local-install/pom.xml ]; then
     echo "Using Java agent from from local folder"
     rm -f /app/elastic-apm-agent.jar
+    # Install xmllint in the alpine
+    apk --no-cache add libxml2-utils
     #Extract current version without using maven which is not available in this image
     JAVA_AGENT_LOCAL_VERSION=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' /local-install/pom.xml)
-    
-    cp -v /local-install/elastic-apm-agent/target/elastic-apm-agent-${JAVA_AGENT_LOCAL_VERSION}.jar /app/elastic-apm-agent.jar
+
+    cp -v "/local-install/elastic-apm-agent/target/elastic-apm-agent-${JAVA_AGENT_LOCAL_VERSION}.jar" /app/elastic-apm-agent.jar
     # copy to folder inside container to ensure were not polluting the local folder
     cp -r /local-install ~
     cd ~/local-install && python setup.py install
     cd -
-elif [[ $JAVA_AGENT_VERSION ]]; then
+elif [ -n "${JAVA_AGENT_VERSION}" ]; then
     echo "Downloading Java agent $JAVA_AGENT_VERSION from maven central"
     rm -f /app/elastic-apm-agent.jar
-    curl -o /app/elastic-apm-agent.jar -L http://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/$JAVA_AGENT_VERSION/elastic-apm-agent-$JAVA_AGENT_VERSION.jar 
+    wget -O /app/elastic-apm-agent.jar -L "http://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/$JAVA_AGENT_VERSION/elastic-apm-agent-$JAVA_AGENT_VERSION.jar"
 else
     echo "Using Java agent from the docker image"
 fi
