8c10ff3384f0c95320ce0e42e89e2c514923f88e
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-10-30 16:27:33 +0100

    fix: add kibana to tests launched on PRs (#660)

--- .ci/scripts/agent.sh
+++ .ci/scripts/agent.sh
@@ -8,6 +8,6 @@ test -z "$srcdir" && srcdir=.
 
 AGENT=$1
 APP=$2
-DEFAULT_COMPOSE_ARGS="${ELASTIC_STACK_VERSION} ${BUILD_OPTS} --with-agent-${APP} --no-apm-server-dashboards --no-apm-server-self-instrument --no-kibana --force-build"
+DEFAULT_COMPOSE_ARGS="${ELASTIC_STACK_VERSION} ${BUILD_OPTS} --with-agent-${APP} --no-apm-server-dashboards --no-apm-server-self-instrument --force-build"
 export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
 runTests "env-agent-${AGENT}" "docker-test-agent-${AGENT}"
