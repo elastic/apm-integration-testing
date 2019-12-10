82ea22b5b6c16314e6f8c4d0d82369c89783ac5b
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2019-12-10 13:50:57 +0100

    fix: cover more whide versions number

--- scripts/modules/apm_agents.py
+++ scripts/modules/apm_agents.py
@@ -218,7 +218,7 @@ class AgentPythonDjango(AgentPython):
             logging=None,
             ports=[self.publish_port(self.port, self.SERVICE_PORT)],
         )
-        if (self.agent_package != 'elastic-apm==5.1' and self.agent_package != 'elastic-apm==4.1'):
+        if ('elastic-apm==5.1' not in self.agent_package and 'elastic-apm==4.' not in self.agent_package):
             ret["environment"]["ELASTIC_APM_VERIFY_SERVER_CERT"] = (
                 str(not self.options.get("no_verify_server_cert")).lower())
         return ret
@@ -247,7 +247,7 @@ class AgentPythonFlask(AgentPython):
             depends_on=self.depends_on,
             ports=[self.publish_port(self.port, self.SERVICE_PORT)],
         )
-        if (self.agent_package != 'elastic-apm==5.1' and self.agent_package != 'elastic-apm==4.1'):
+        if ('elastic-apm==5.1' not in self.agent_package and 'elastic-apm==4.' not in self.agent_package):
             ret["environment"]["ELASTIC_APM_VERIFY_SERVER_CERT"] = (
                 str(not self.options.get("no_verify_server_cert")).lower())
         return ret
