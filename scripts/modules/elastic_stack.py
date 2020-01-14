a11c762d243f9076feacdb6aebc50659471ad188
Victor Martinez <victormartinezrubio@gmail.com>
2020-01-14 13:47:11 +0000

    Customise Kibana login with an assistance message (#702)

--- scripts/modules/elastic_stack.py
+++ scripts/modules/elastic_stack.py
@@ -656,6 +656,11 @@ class Kibana(StackService, Service):
                 self.environment["STATUS_ALLOWANONYMOUS"] = "true"
         self.environment["ELASTICSEARCH_URL"] = ",".join(self.options.get(
             "kibana_elasticsearch_urls") or [self.DEFAULT_ELASTICSEARCH_HOSTS])
+        if self.at_least_version("7.6") and options.get("xpack_secure"):
+            self.environment["XPACK_SECURITY_LOGINASSISTANCEMESSAGE"] = "Login&#32;details:&#32;`{}/{}`.&#32;" \
+                "Further&#32;details&#32;[here]({}).".format(
+                    self.environment["ELASTICSEARCH_USERNAME"], self.environment["ELASTICSEARCH_PASSWORD"],
+                    "https://github.com/elastic/apm-integration-testing#logging-in")
 
     @classmethod
     def add_arguments(cls, parser):
