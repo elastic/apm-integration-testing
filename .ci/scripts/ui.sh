1e8e2c03d55ec37b5c025e58e7cee18da56731e4
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-01-17 14:29:02 +0100

    fix: set MERGE_TARGET as branch instead of INTEGRATION_TESTING_VERSION (#717) (#719)

--- .ci/scripts/ui.sh
+++ .ci/scripts/ui.sh
@@ -3,4 +3,4 @@
 
 cd scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs
 yarn
-yarn setup elastic "${INTEGRATION_TESTING_VERSION}" elastic "${INTEGRATION_TESTING_VERSION}" && yarn lint
+yarn setup elastic "${MERGE_TARGET}" elastic "${MERGE_TARGET}" && yarn lint
