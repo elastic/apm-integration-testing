2139bcf8c657d3ef3efbf758cba3073298645146
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-21 12:04:10 +0000

    lint: fix lint defects

--- scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs/scripts/clone-kibana.sh
+++ scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs/scripts/clone-kibana.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
 OWNER=${1:-elastic}
 BRANCH=${2:-master}
 
-echo "Cloning Kibana: $OWNER:$BRANCH"
+echo "Cloning Kibana: ${OWNER}:${BRANCH}"
 
-cd ./tmp
-git clone --quiet --depth 1 -b $BRANCH https://github.com/$OWNER/kibana.git
+cd ./tmp || true
+git clone --quiet --depth 1 -b "${BRANCH}" "https://github.com/${OWNER}/kibana.git"
 mv ./kibana/x-pack/plugins/apm/typings/es_schemas ./apm-ui-interfaces
 rm -rf kibana
