084fc6793ca3ae156299cc465c3833eac084e05f
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-24 14:36:17 +0000

    as we agreed we won't need this particular docs in the ES build data instance

--- .ci/integrationTestDownstream.groovy
+++ .ci/integrationTestDownstream.groovy
@@ -145,11 +145,6 @@ pipeline {
           def processor = new ResultsProcessor()
           processor.processResults(mapResults)
           archiveArtifacts allowEmptyArchive: true, artifacts: 'results.json,results.html', defaultExcludes: false
-          catchError(buildResult: 'SUCCESS') {
-            def datafile = readFile(file: "results.json")
-            def json = getVaultSecret(secret: 'secret/apm-team/ci/jenkins-stats-cloud')
-            sendDataToElasticsearch(es: json.data.url, data: datafile, restCall: '/jenkins-builds-it-results/_doc/')
-          }
         }
         notifyBuildResult()
       }
