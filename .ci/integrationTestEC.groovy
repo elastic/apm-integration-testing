cd2af880fb432568c58e03c3c2eac157aafa7382
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-03-09 11:31:47 +0100

    test: reduce axis

--- .ci/integrationTestEC.groovy
+++ .ci/integrationTestEC.groovy
@@ -47,6 +47,17 @@ pipeline {
     stage('Tests On Elastic Cloud'){
       matrix {
         agent { label 'linux && immutable' }
+        environment {
+          TMPDIR = "${env.WORKSPACE}"
+          REUSE_CONTAINERS = "true"
+          HOME = "${env.WORKSPACE}"
+          CONFIG_HOME = "${env.WORKSPACE}"
+          EC_WS ="${env.WORKSPACE}/${env.EC_DIR}"
+          VENV = "${env.WORKSPACE}/.venv"
+          PATH = "${env.WORKSPACE}/${env.BASE_DIR}/.ci/scripts:${env.VENV}/bin:${env.EC_WS}/bin:${env.EC_WS}/.ci/scripts:${env.PATH}"
+          CLUSTER_CONFIG_FILE="${env.EC_WS}/tests/environments/elastic_cloud.yml"
+          ENABLE_ES_DUMP = "true"
+        }
         axes {
           axis {
               name 'TEST'
