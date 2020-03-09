8de74aa9930d63b1b7c8f2e60f70793b1cf4f3b6
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-03-09 11:35:52 +0100

    test: reduce axis

--- .ci/integrationTestEC.groovy
+++ .ci/integrationTestEC.groovy
@@ -47,17 +47,6 @@ pipeline {
     stage('Tests On Elastic Cloud'){
       matrix {
         agent { label 'linux && immutable' }
-        environment {
-          TMPDIR = "${env.WORKSPACE}"
-          REUSE_CONTAINERS = "true"
-          HOME = "${env.WORKSPACE}"
-          CONFIG_HOME = "${env.WORKSPACE}"
-          EC_WS ="${env.WORKSPACE}/${env.EC_DIR}"
-          VENV = "${env.WORKSPACE}/.venv"
-          PATH = "${env.WORKSPACE}/${env.BASE_DIR}/.ci/scripts:${env.VENV}/bin:${env.EC_WS}/bin:${env.EC_WS}/.ci/scripts:${env.PATH}"
-          CLUSTER_CONFIG_FILE="${env.EC_WS}/tests/environments/elastic_cloud.yml"
-          ENABLE_ES_DUMP = "true"
-        }
         axes {
           axis {
               name 'TEST'
@@ -70,6 +59,17 @@ pipeline {
         }
         stages {
           stage('Prepare Test'){
+            environment {
+              TMPDIR = "${env.WORKSPACE}"
+              REUSE_CONTAINERS = "true"
+              HOME = "${env.WORKSPACE}"
+              CONFIG_HOME = "${env.WORKSPACE}"
+              EC_WS ="${env.WORKSPACE}/${env.EC_DIR}"
+              VENV = "${env.WORKSPACE}/.venv"
+              PATH = "${env.WORKSPACE}/${env.BASE_DIR}/.ci/scripts:${env.VENV}/bin:${env.EC_WS}/bin:${env.EC_WS}/.ci/scripts:${env.PATH}"
+              CLUSTER_CONFIG_FILE="${env.EC_WS}/tests/environments/elastic_cloud.yml"
+              ENABLE_ES_DUMP = "true"
+            }
             steps {
               log(level: "INFO", text: "Running tests - ${ELASTIC_STACK_VERSION} x ${TEST}")
               deleteDir()
