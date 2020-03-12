b78e59016bc61ef258ecbf68034b4b52708b9f6e
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2020-03-12 16:01:44 +0100

    fix: remove 6.8 versions

--- .ci/integrationTestEC.groovy
+++ .ci/integrationTestEC.groovy
@@ -55,7 +55,7 @@ pipeline {
         axes {
           axis {
               name 'ELASTIC_STACK_VERSION'
-              values '8.0.0-SNAPSHOT', '7.7.0-SNAPSHOT', '7.6.1-SNAPSHOT', '6.8.7-SNAPSHOT'
+              values '8.0.0-SNAPSHOT', '7.7.0-SNAPSHOT', '7.6.1-SNAPSHOT'
           }
         }
         stages {
