00a6af332fce9c789499ee5332903aa3baf13f7b
Mike Place <mike.place@elastic.co>
2020-05-25 12:48:59 +0200

    Fix path for sourcemap file

--- scripts/modules/cli.py
+++ scripts/modules/cli.py
@@ -689,7 +689,7 @@ class LocalSetup(object):
             '-F service_name="{service_name}" '
             '-F service_version="{service_version}" '
             '-F bundle_filepath="{bundle_path}" '
-            '-F sourcemap=@/tmp/sourcemap '
+            '-F sourcemap=@{sourcemap_file} '
             '{auth_header}'
             '{server_url}/v1/client-side/sourcemaps'
         ).format(
@@ -700,6 +700,7 @@ class LocalSetup(object):
             auth_header=auth_header,
             server_url=self.args.apm_server_url,
         )
+        print(cmd)
         cmd = "docker run --rm --network apm-integration-testing " + \
               "-v {}:/tmp/sourcemap centos:7 ".format(sourcemap_file) + cmd
         subprocess.check_output(cmd, shell=True).decode('utf8').strip()
