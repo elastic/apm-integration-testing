9239c31e838c9edd58174575c3ba833eb1c4be6a
Victor Martinez <victormartinezrubio@gmail.com>
2020-06-08 13:13:41 +0100

    feat: enable opbeans customisation (#556) (#857)

---
+++ .ci/scripts/opbeans.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+# for details about how it works see https://github.com/elastic/apm-integration-testing#continuous-integration
+
+srcdir=$(dirname "${0}")
+test -z "$srcdir" && srcdir=.
+# shellcheck disable=SC1090
+. ${srcdir}/common.sh
+
+DEFAULT_COMPOSE_ARGS="${ELASTIC_STACK_VERSION} ${BUILD_OPTS} --no-apm-server-dashboards --no-apm-server-self-instrument --no-kibana --force-build"
+export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
+runTests env-server test-compose lint test-helps
