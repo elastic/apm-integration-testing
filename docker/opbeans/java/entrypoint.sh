8612917355b68f86e36dc6d2debc7ee4487468e0
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-09-15 09:37:20 +0200

    fix: increase the resilience against network issues (#927)
    * fix: increase the resilence against network issues
    
    * Apply suggestions from code review

--- docker/opbeans/java/entrypoint.sh
+++ docker/opbeans/java/entrypoint.sh
@@ -4,8 +4,8 @@ if [[ -f /local-install/pom.xml ]]; then
     rm -f /app/elastic-apm-agent.jar
     #Extract current version without using maven which is not available in this image
     JAVA_AGENT_LOCAL_VERSION=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' /local-install/pom.xml)
-    
-    cp -v /local-install/elastic-apm-agent/target/elastic-apm-agent-${JAVA_AGENT_LOCAL_VERSION}.jar /app/elastic-apm-agent.jar
+
+    cp -v "/local-install/elastic-apm-agent/target/elastic-apm-agent-${JAVA_AGENT_LOCAL_VERSION}.jar" /app/elastic-apm-agent.jar
     # copy to folder inside container to ensure were not polluting the local folder
     cp -r /local-install ~
     cd ~/local-install && python setup.py install
@@ -13,7 +13,7 @@ if [[ -f /local-install/pom.xml ]]; then
 elif [[ $JAVA_AGENT_VERSION ]]; then
     echo "Downloading Java agent $JAVA_AGENT_VERSION from maven central"
     rm -f /app/elastic-apm-agent.jar
-    curl -o /app/elastic-apm-agent.jar -L http://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/$JAVA_AGENT_VERSION/elastic-apm-agent-$JAVA_AGENT_VERSION.jar 
+    curl -o /app/elastic-apm-agent.jar -L "https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/$JAVA_AGENT_VERSION/elastic-apm-agent-$JAVA_AGENT_VERSION.jar"
 else
     echo "Using Java agent from the docker image"
 fi
