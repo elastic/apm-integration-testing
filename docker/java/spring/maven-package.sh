69be5203bec92f14c951d18628dbef41bca7144a
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-09-16 15:33:08 +0200

    [7.x] fix: increase the resilience against network issues (#927) (#928)
    * fix: increase the resilience against network issues (#927)
    
    * fix: increase the resilence against network issues
    
    * Apply suggestions from code review
    
    * Update docker/java/spring/maven-package.sh

--- docker/java/spring/maven-package.sh
+++ docker/java/spring/maven-package.sh
@@ -11,6 +11,8 @@ if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
     -DskipTests=true \
     -Dhttps.protocols=TLSv1.2 \
     -Dmaven.javadoc.skip=true \
+    -Dmaven.wagon.http.retryHandler.count=3 \
+    -Dhttp.keepAlive=false \
     -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
   # shellcheck disable=SC2016
   JAVA_AGENT_BUILT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
@@ -18,8 +20,10 @@ if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
 else
   mvn -q --batch-mode org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
       -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
-      -DrepoUrl=http://repo1.maven.apache.org/maven2 \
       -Dhttps.protocols=TLSv1.2 \
+      -DrepoUrl=https://repo1.maven.apache.org/maven2 \
+      -Dmaven.wagon.http.retryHandler.count=3 \
+      -Dhttp.keepAlive=false \
       -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}"
 fi
 
@@ -29,6 +33,8 @@ mvn -q --batch-mode -DAGENT_API_VERSION="${JAVA_AGENT_BUILT_VERSION}" \
   -DskipTests=true \
   -Dhttps.protocols=TLSv1.2 \
   -Dmaven.javadoc.skip=true \
+  -Dmaven.wagon.http.retryHandler.count=3 \
+  -Dhttp.keepAlive=false \
   package
 
 cp "/root/.m2/repository/co/elastic/apm/${ARTIFACT_ID}/${JAVA_AGENT_BUILT_VERSION}/${ARTIFACT_ID}-${JAVA_AGENT_BUILT_VERSION}.jar" /agent/apm-agent.jar
