23fca760931b507892827810dd36d9b9adcdfb19
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2020-10-21 20:48:03 +0300

    Added stack depth to conform to 'assert 15 < len(stacktrace) < 70'
    (cherry picked from commit 44983ffcd1d9e4ed3d1d045baacd39f8f044fe41)
    
    # Conflicts:
    #	docker/php/apache/app/foo/index.php

--- docker/php/apache/app/foo/index.php
+++ docker/php/apache/app/foo/index.php
@@ -14,12 +14,21 @@ function dummyFuncToAddDepthToStacktrace(int $depthLeft): void
     global $span;
 
     // It's 2 because current dummyFuncToAddDepthToStacktrace frame
+<<<<<<< HEAD
     // and frame for call to $span->end() below are included in span's stacktrace
     if ($depthLeft > 2) {
         dummyFuncToAddDepthToStacktrace($depthLeft - 1);
     } else {
         $span->end();
     }
+=======
+    // and $span->end() frame are counted
+    if ($depthLeft > 2) {
+        dummyFuncToAddDepthToStacktrace($depthLeft - 1);
+    }
+
+    $span->end();
+>>>>>>> 44983ff (Added stack depth to conform to 'assert 15 < len(stacktrace) < 70')
 }
 
 dummyFuncToAddDepthToStacktrace(16);
