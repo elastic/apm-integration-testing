44983ffcd1d9e4ed3d1d045baacd39f8f044fe41
Sergey Kleyman <SergeyKleyman@users.noreply.github.com>
2020-10-21 20:48:03 +0300

    Added stack depth to conform to 'assert 15 < len(stacktrace) < 70'

--- docker/php/apache/app/foo/index.php
+++ docker/php/apache/app/foo/index.php
@@ -4,10 +4,23 @@ use Elastic\Apm\ElasticApm;
 $transaction = ElasticApm::getCurrentTransaction();
 $span = $transaction->beginCurrentSpan(
     'foo',
-    'app',
+    'app'
 );
 ?>
 foo
 <?php
-$span->end();
+function dummyFuncToAddDepthToStacktrace(int $depthLeft): void
+{
+    global $span;
+
+    // It's 2 because current dummyFuncToAddDepthToStacktrace frame
+    // and $span->end() frame are counted
+    if ($depthLeft > 2) {
+        dummyFuncToAddDepthToStacktrace($depthLeft - 1);
+    }
+
+    $span->end();
+}
+
+dummyFuncToAddDepthToStacktrace(16);
 ?>
