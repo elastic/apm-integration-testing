4738b4b903402c7ca019fef72110d7e852d811e9
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2020-10-27 20:27:12 +0100

    chore: update stack versions (#952)

--- .ci/integrationTestEC.groovy
+++ .ci/integrationTestEC.groovy
@@ -56,7 +56,7 @@ pipeline {
         axes {
           axis {
               name 'ELASTIC_STACK_VERSION'
-              values '8.0.0-SNAPSHOT', '7.10.0-SNAPSHOT', '7.9.2'
+              values '8.0.0-SNAPSHOT', '7.11.0-SNAPSHOT', '7.10.0', '7.9.3'
           }
         }
         stages {
