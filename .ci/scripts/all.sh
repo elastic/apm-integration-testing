6f6d1d649c28fbd9a19ba49f2321ebd567206dd5
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-17 11:11:51 +0000

    Modify scripts to configure the m2 repository

--- .ci/scripts/all.sh
+++ .ci/scripts/all.sh
@@ -11,6 +11,12 @@ export ELASTIC_APM_SECRET_TOKEN=${ELASTIC_APM_SECRET_TOKEN:-"SuPeRsEcReT"}
 export APM_SERVER_URL=${APM_SERVER_URL:-"https://apm-server:8200"}
 export PYTHONHTTPSVERIFY=0
 
+## This is for the CI
+if [ -d /var/lib/jenkins/.m2/repository ] ; then
+  cp -rf /var/lib/jenkins/.m2/repositor docker/java/spring/.m2
+  BUILD_OPTS="${BUILD_OPTS} --java-m2-cache docker/java/spring/.m2"
+fi
+
 DEFAULT_COMPOSE_ARGS="${ELASTIC_STACK_VERSION} ${BUILD_OPTS}\
   --no-apm-server-dashboards \
   --no-apm-server-self-instrument \
