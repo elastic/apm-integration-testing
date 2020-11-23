ee8d0876e9e57eb40c106c1b13028c909e10151a
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-11-23 10:32:08 +0000

    Force variable since it uses mvn instead the mvnw

--- docker/java/spring/maven-package.sh
+++ docker/java/spring/maven-package.sh
@@ -17,7 +17,8 @@ if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
     -Dmaven.javadoc.skip=true \
     -Dmaven.wagon.http.retryHandler.count=10 \
     -Dhttp.keepAlive=false \
-    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+    ${MAVEN_CONFIG}
   # shellcheck disable=SC2016
   JAVA_AGENT_BUILT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   export JAVA_AGENT_BUILT_VERSION="${JAVA_AGENT_BUILT_VERSION}"
@@ -28,7 +29,8 @@ else
       -DrepoUrl=https://repo1.maven.apache.org/maven2 \
       -Dmaven.wagon.http.retryHandler.count=10 \
       -Dhttp.keepAlive=false \
-      -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}"
+      -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}" \
+      ${MAVEN_CONFIG}
 fi
 
 cd /app
@@ -39,6 +41,7 @@ mvn -q --batch-mode -DAGENT_API_VERSION="${JAVA_AGENT_BUILT_VERSION}" \
   -Dmaven.javadoc.skip=true \
   -Dmaven.wagon.http.retryHandler.count=10 \
   -Dhttp.keepAlive=false \
+  ${MAVEN_CONFIG} \
   package
 
 cp "/root/.m2/repository/co/elastic/apm/${ARTIFACT_ID}/${JAVA_AGENT_BUILT_VERSION}/${ARTIFACT_ID}-${JAVA_AGENT_BUILT_VERSION}.jar" /agent/apm-agent.jar
