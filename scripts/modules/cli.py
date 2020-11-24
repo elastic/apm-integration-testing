ad95a2226472bb45b118e00c1a4741adf85314c1
Mike Place <mike.place@elastic.co>
2020-11-24 10:28:00 +0100

    Remove outdated comment

--- scripts/modules/cli.py
+++ scripts/modules/cli.py
@@ -589,9 +589,6 @@ class LocalSetup(object):
         if args.get('dyno'):
             toxi = Toxi()
             selections.add(toxi)
-            # 1. Create configuration that +1s all the service ports and writes config
-            # 1.1 Mount the config somewhere that the container can see it.
-            # 2. TODO: Flip the logic in step 1 to preserve the original ports as proxied and to shift the true service ports
             c = toxi.gen_config(selections)
             with open('../docker/toxi/toxi.cfg', 'w') as fh_:
                 fh_.write(c)
