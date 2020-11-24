2cd59ab80376a1f3e9ee94b850ccd8f7eeecb804
Victor Martinez <victormartinezrubio@gmail.com>
2020-11-24 09:24:50 +0000

    Use cached .m2 dependencies (#983)

--- docker/java/spring/maven-package.sh
+++ docker/java/spring/maven-package.sh
@@ -4,16 +4,22 @@ JAVA_AGENT_BUILT_VERSION=${1}
 
 ARTIFACT_ID=elastic-apm-agent
 
+export M2_REPOSITORY_FOLDER=/root/.m2/repository
+if [ "${JAVA_M2_CACHE}" == "true" ] ; then
+  export MAVEN_CONFIG="-Dmaven.repo.local=${M2_REPOSITORY_FOLDER}"
+fi
+
 if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
   cd /agent/apm-agent-java
-  git log -1
+  git --no-pager log -1
   mvn -q --batch-mode clean install \
     -DskipTests=true \
     -Dhttps.protocols=TLSv1.2 \
     -Dmaven.javadoc.skip=true \
     -Dmaven.wagon.http.retryHandler.count=10 \
     -Dhttp.keepAlive=false \
-    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+    -Dmaven.repo.local=${M2_REPOSITORY_FOLDER}
   # shellcheck disable=SC2016
   JAVA_AGENT_BUILT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   export JAVA_AGENT_BUILT_VERSION="${JAVA_AGENT_BUILT_VERSION}"
@@ -24,7 +30,8 @@ else
       -DrepoUrl=https://repo1.maven.apache.org/maven2 \
       -Dmaven.wagon.http.retryHandler.count=10 \
       -Dhttp.keepAlive=false \
-      -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}"
+      -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}" \
+      -Dmaven.repo.local=${M2_REPOSITORY_FOLDER}
 fi
 
 cd /app
@@ -35,6 +42,7 @@ mvn -q --batch-mode -DAGENT_API_VERSION="${JAVA_AGENT_BUILT_VERSION}" \
   -Dmaven.javadoc.skip=true \
   -Dmaven.wagon.http.retryHandler.count=10 \
   -Dhttp.keepAlive=false \
+  -Dmaven.repo.local=${M2_REPOSITORY_FOLDER} \
   package
 
-cp "/root/.m2/repository/co/elastic/apm/${ARTIFACT_ID}/${JAVA_AGENT_BUILT_VERSION}/${ARTIFACT_ID}-${JAVA_AGENT_BUILT_VERSION}.jar" /agent/apm-agent.jar
+cp "${M2_REPOSITORY_FOLDER}/co/elastic/apm/${ARTIFACT_ID}/${JAVA_AGENT_BUILT_VERSION}/${ARTIFACT_ID}-${JAVA_AGENT_BUILT_VERSION}.jar" /agent/apm-agent.jar
