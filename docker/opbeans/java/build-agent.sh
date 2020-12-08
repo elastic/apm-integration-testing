b2676cb7b769c59307e171217fffe7a5809b7206
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-12-08 09:25:21 +0000

    Download maven dependencies firstly

--- docker/opbeans/java/build-agent.sh
+++ docker/opbeans/java/build-agent.sh
@@ -12,12 +12,13 @@ if [ -n "${JAVA_AGENT_BRANCH}" ] ; then
   git fetch -q origin '+refs/pull/*:refs/remotes/origin/pr/*'
   git checkout "${JAVA_AGENT_BRANCH}"
 
+  mvn dependency:go-offline --fail-never -q -B
   mvn -q --batch-mode clean install \
     -DskipTests=true \
     -Dhttps.protocols=TLSv1.2 \
     -Dmaven.javadoc.skip=true \
-    -Dmaven.wagon.http.retryHandler.count=3 \
-    -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.retryHandler.count=10 \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 \
     -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
   # shellcheck disable=SC2016
   VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
