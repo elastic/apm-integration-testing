744e367319933f6770753a564ba746810ada54f9
Victor Martinez <victormartinezrubio@gmail.com>
2020-12-17 11:44:13 +0000

    [java] Build with another retry if something bad happens (#1008)

--- docker/java/spring/maven-package.sh
+++ docker/java/spring/maven-package.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -xe
 JAVA_AGENT_BUILT_VERSION=${1}
-
 ARTIFACT_ID=elastic-apm-agent
+
+function mavenRun() {
+  mvn -q --batch-mode \
+    -DskipTests=true \
+    -Dmaven.javadoc.skip=true \
+    -Dhttps.protocols=TLSv1.2 \
+    -Dmaven.wagon.http.retryHandler.count=10 \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 \
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+    -Dmaven.repo.local="${M2_REPOSITORY_FOLDER}" \
+    "$@"
+}
 
 export M2_REPOSITORY_FOLDER=/root/.m2/repository
 if [ "${JAVA_M2_CACHE}" == "true" ] ; then
@@ -13,37 +24,26 @@ if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
   cd /agent/apm-agent-java
   git --no-pager log -1
   mvn dependency:go-offline --fail-never -q -B
-  mvn -q --batch-mode clean install \
-    -DskipTests=true \
-    -Dmaven.javadoc.skip=true \
-    -Dhttps.protocols=TLSv1.2 \
-    -Dmaven.wagon.http.retryHandler.count=10 \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 \
-    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
-    -Dmaven.repo.local=${M2_REPOSITORY_FOLDER}
+  if ! mavenRun clean install ; then
+    echo 'Sleep and try again'
+    sleep 5
+    mavenRun install
+  fi
   # shellcheck disable=SC2016
   JAVA_AGENT_BUILT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   export JAVA_AGENT_BUILT_VERSION="${JAVA_AGENT_BUILT_VERSION}"
 else
-  mvn -q --batch-mode org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
-      -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
-      -Dhttps.protocols=TLSv1.2 \
+ mavenRun org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
       -DrepoUrl=https://repo1.maven.apache.org/maven2 \
-      -Dmaven.wagon.http.retryHandler.count=10 \
-      -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 \
-      -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}" \
-      -Dmaven.repo.local=${M2_REPOSITORY_FOLDER}
+      -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}"
 fi
 
 cd /app
-mvn -q --batch-mode -DAGENT_API_VERSION="${JAVA_AGENT_BUILT_VERSION}" \
-  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
-  -DskipTests=true \
-  -Dmaven.javadoc.skip=true \
-  -Dhttps.protocols=TLSv1.2 \
-  -Dmaven.wagon.http.retryHandler.count=10 \
-  -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 \
-  -Dmaven.repo.local=${M2_REPOSITORY_FOLDER} \
-  package
+
+if ! mavenRun package -DAGENT_API_VERSION="${JAVA_AGENT_BUILT_VERSION}" ; then
+  echo 'Sleep and try again'
+  sleep 5
+  mavenRun package -DAGENT_API_VERSION="${JAVA_AGENT_BUILT_VERSION}"
+fi
 
 cp "${M2_REPOSITORY_FOLDER}/co/elastic/apm/${ARTIFACT_ID}/${JAVA_AGENT_BUILT_VERSION}/${ARTIFACT_ID}-${JAVA_AGENT_BUILT_VERSION}.jar" /agent/apm-agent.jar
