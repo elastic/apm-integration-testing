5efe507a42ed658c13529dc3cabea22af64e54e8
Victor Martinez <victormartinezrubio@gmail.com>
2021-01-04 11:32:01 +0000

    Update docker/java/spring/maven-package.sh

--- docker/java/spring/maven-package.sh
+++ docker/java/spring/maven-package.sh
@@ -40,7 +40,7 @@ if [ -z "${JAVA_AGENT_BUILT_VERSION}" ] ; then
   JAVA_AGENT_BUILT_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   export JAVA_AGENT_BUILT_VERSION="${JAVA_AGENT_BUILT_VERSION}"
 else
-   mavenRun org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
+  mavenRun org.apache.maven.plugins:maven-dependency-plugin:2.1:get \
       -DrepoUrl=https://repo1.maven.apache.org/maven2 \
       -Dartifact="co.elastic.apm:${ARTIFACT_ID}:${JAVA_AGENT_BUILT_VERSION}"
 fi
