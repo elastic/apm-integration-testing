b0175f4f8efbd16bc95dae94d5871d976c2a45dd
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-01-20 12:12:43 +0100

    ci: backport docker folder

---
+++ docker/apm-server/managed/go.mod
@@ -0,0 +1,5 @@
+module managed
+
+go 1.15
+
+require github.com/pkg/errors v0.9.2-0.20201214064552-5dd12d0cfe7f
--- docker/opbeans/node/entrypoint.sh
+++ docker/opbeans/node/entrypoint.sh
@@ -1,24 +1,22 @@
-#!/bin/bash -e
-
-set -x
-
-if [[ -f /local-install/package.json ]]; then
+#!/usr/bin/env sh
+set -ex
+if [ -f /local-install/package.json ]; then
     echo "Installing from local folder"
     # copy to folder inside container to ensure were not poluting the local folder
     cp -r /local-install ~
     cd ~/local-install && npm install .
     cd -
-elif [[ $NODE_AGENT_VERSION ]]; then
+elif [ -n "${NODE_AGENT_VERSION}" ]; then
     echo "Installing ${NODE_AGENT_VERSION} from npm"
     npm install elastic-apm-node@"${NODE_AGENT_VERSION}"
-elif [[ $NODE_AGENT_BRANCH ]]; then
-    if [[ -z ${NODE_AGENT_REPO} ]]; then
+elif [ -n "${NODE_AGENT_BRANCH}" ]; then
+    if [ -z "${NODE_AGENT_REPO}" ]; then
         NODE_AGENT_REPO="elastic/apm-agent-nodejs"
     fi
     echo "Installing ${NODE_AGENT_REPO}:${NODE_AGENT_BRANCH} from Github"
-    npm install https://github.com/${NODE_AGENT_REPO}/archive/${NODE_AGENT_BRANCH}.tar.gz
+    npm install "https://github.com/${NODE_AGENT_REPO}/archive/${NODE_AGENT_BRANCH}.tar.gz"
 fi
-if [[ -f /sourcemaps/README.md ]]; then
+if [ -f /sourcemaps/README.md ]; then
     rm -f /sourcemaps/*.map
     cp -f ./client/build/static/js/*.map /sourcemaps/
     chmod 0666 /sourcemaps/*.map
