ddbb9cbb66e838a160a56353fe6fcbe3b63d297d
kuisathaverat <kuisathaverat@yahoo.com>
2021-02-16 11:18:47 +0100

    chore: bump Elastic Stack

--- .ci/integrationTestEC.groovy
+++ .ci/integrationTestEC.groovy
@@ -56,7 +56,7 @@ pipeline {
         axes {
           axis {
               name 'ELASTIC_STACK_VERSION'
-              values '8.0.0-SNAPSHOT', '7.12.0-SNAPSHOT', '7.11.0-SNAPSHOT', '7.10.2'
+              values '8.0.0-SNAPSHOT', '7.12.0-SNAPSHOT', '7.11.1'
           }
         }
         stages {
