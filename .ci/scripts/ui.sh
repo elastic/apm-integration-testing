8f7a8535142c667855aaa90c67e153f16061dd5d
cachedout <mike.place@elastic.co>
2021-05-06 09:40:28 +0000

    Implement retries in yarn commands (#1138)

--- .ci/scripts/ui.sh
+++ .ci/scripts/ui.sh
@@ -2,5 +2,27 @@
 # for details about how it works see https://github.com/elastic/apm-integration-testing#continuous-integration
 
 cd scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs
-yarn
-yarn setup elastic "${MERGE_TARGET}" elastic "${MERGE_TARGET}" && yarn lint
+
+n=0
+until [ "$n" -ge 5 ]
+do
+   yarn  && break  # substitute your command here
+   n=$((n+1))
+   sleep 15
+done
+
+n=0
+until [ "$n" -ge 5 ]
+do
+    yarn setup elastic "${MERGE_TARGET}" elastic "${MERGE_TARGET}" && break
+   n=$((n+1))
+   sleep 15
+done
+
+n=0
+until [ "$n" -ge 5 ]
+do
+   yarn lint && break  # substitute your command here
+   n=$((n+1))
+   sleep 15
+done
