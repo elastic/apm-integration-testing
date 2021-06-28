fbd2c898d6b28dc6d15f7cf090829ddab9ab7880
Victor Martinez <victormartinezrubio@gmail.com>
2021-06-28 14:27:26 +0100

    Validate the pre-requirements (#1185)
    (cherry picked from commit 212fdf6b9f18d8dba4524d7403ac05715ed0ec4a)

--- scripts/compose.py
+++ scripts/compose.py
@@ -3,6 +3,8 @@
 from __future__ import print_function
 
 import logging
+import os
+import subprocess
 import sys
 
 from modules.cli import LocalSetup
@@ -15,5 +17,15 @@ def main():
     setup()
 
 
+def verify_if_docker_is_installed():
+    try:
+        subprocess.check_output(
+            'docker ps', stderr=open(os.devnull, 'w'), shell=True).decode('utf8').strip()
+    except subprocess.CalledProcessError:
+        print("Please start Docker before running the apm-integration-testing.")
+        sys.exit(1)
+
+
 if __name__ == '__main__':
+    verify_if_docker_is_installed()
     main()
