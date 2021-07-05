6404328c7b0be7560adbdc2a9c96051f15ae2019
Victor Martinez <VictorMartinezRubio@gmail.com>
2021-07-05 12:33:22 +0100

    Change docker validation

--- scripts/compose.py
+++ scripts/compose.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 
 import logging
-import os
 import subprocess
 import sys
 
@@ -19,9 +18,8 @@ def main():
 
 def verify_if_docker_is_installed():
     try:
-        subprocess.check_output(
-            'docker ps', stderr=open(os.devnull, 'w'), shell=True).decode('utf8').strip()
-    except subprocess.CalledProcessError:
+        subprocess.check_call('docker ps')
+    except OSError as err:
         print("Please start Docker before running the apm-integration-testing.")
         sys.exit(1)
 
