db407c095cf702458fba017e0bd90b3bc3af812f
Gil Raphaelli <gil@elastic.co>
2021-08-17 12:32:33 -0400

    respect kibana tls setting

--- scripts/modules/elastic_stack.py
+++ scripts/modules/elastic_stack.py
@@ -1028,7 +1028,6 @@ class EnterpriseSearch(StackService, Service):
         self.environment = {
             "allow_es_settings_modification": "true",
             "ent_search.external_url": "http://localhost:{}".format(self.port),
-            "kibana.external_url": "http://localhost:5601",
             "secret_management.encryption_keys": '[4a2cd3f81d39bf28738c10db0ca782095ffac07279561809eecc722e0c20eb09]',
             "apm.enabled": "true",
             "apm.server_url": options.get("apm_server_url", DEFAULT_APM_SERVER_URL),
@@ -1042,6 +1041,10 @@ class EnterpriseSearch(StackService, Service):
         es_urls = self.options.get("enterprise_search_elasticsearch_urls") or \
             [self.default_elasticsearch_hosts(self.es_tls)]
         self.environment["elasticsearch.host"] = es_urls[0]
+        kibana_scheme = "https" if self.kibana_tls else "http"
+        self.environment.update({
+            "kibana.external_url": kibana_scheme + "://localhost:5601",
+        })
 
         default_creds = {"username": "admin", "password": "changeme"}
         for cfg in ("username", "password"):
