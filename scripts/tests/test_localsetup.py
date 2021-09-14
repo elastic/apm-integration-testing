82a3dd3af29fd9172302ffaa7a2a44e74d8202e8
cachedout <mike.place@elastic.co>
2021-09-14 14:04:49 +0200

    Sampling rate overhaul (#1255)
    * Add CLI argument for --drop-unsampled
    
    * Wire up drop_unsampled to apm-server.sampling.keep_unsampled
    
    * Version detection for default of sampling
    
    * Various pytest fixes
    
    * Adding pytest files with standard names
    
    * Minor unrelated fixups
    
    * More minor fixups
    
    * Test fixup and adding per-opbean sampling rates
    
    * Simple validation
    
    * Fix lint error
    
    * Fix pytest commands to point to proper test names
    
    * Test fixup

--- scripts/tests/test_localsetup.py
+++ scripts/tests/test_localsetup.py
@@ -19,7 +19,7 @@ from ..modules.opbeans import (
 
 from ..modules.cli import discover_services, LocalSetup
 
-from .service_tests import ServiceTest
+from .test_service import ServiceTest
 
 try:
     import unittest.mock as mock
@@ -70,10 +70,10 @@ class OpbeansServiceTest(ServiceTest):
                       - ELASTIC_APM_VERIFY_SERVER_CERT=true
                       - ELASTIC_APM_FLUSH_INTERVAL=5
                       - ELASTIC_APM_TRANSACTION_MAX_SPANS=50
-                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=1
                       - ELASTICSEARCH_URL=http://elasticsearch:9200
                       - OPBEANS_DT_PROBABILITY=0.50
                       - ELASTIC_APM_ENVIRONMENT=production
+                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.10
                     logging:
                       driver: 'json-file'
                       options:
@@ -132,7 +132,6 @@ class OpbeansServiceTest(ServiceTest):
                       - ELASTIC_APM_VERIFY_SERVER_CERT=true
                       - ELASTIC_APM_FLUSH_INTERVAL=5
                       - ELASTIC_APM_TRANSACTION_MAX_SPANS=50
-                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=1
                       - ELASTICSEARCH_URL=http://elasticsearch:9200
                       - OPBEANS_CACHE=redis://redis:6379
                       - OPBEANS_PORT=3000
@@ -143,6 +142,7 @@ class OpbeansServiceTest(ServiceTest):
                       - PGSSLMODE=disable
                       - OPBEANS_DT_PROBABILITY=0.50
                       - ELASTIC_APM_ENVIRONMENT=production
+                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.10
                     logging:
                       driver: 'json-file'
                       options:
@@ -198,7 +198,6 @@ class OpbeansServiceTest(ServiceTest):
                       - ELASTIC_APM_VERIFY_SERVER_CERT=true
                       - ELASTIC_APM_FLUSH_INTERVAL=5
                       - ELASTIC_APM_TRANSACTION_MAX_SPANS=50
-                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=1
                       - ELASTIC_APM_ENABLE_LOG_CORRELATION=true
                       - DATABASE_URL=jdbc:postgresql://postgres/opbeans?user=postgres&password=verysecure
                       - DATABASE_DIALECT=POSTGRESQL
@@ -209,6 +208,7 @@ class OpbeansServiceTest(ServiceTest):
                       - JAVA_AGENT_VERSION
                       - OPBEANS_DT_PROBABILITY=0.50
                       - ELASTIC_APM_ENVIRONMENT=production
+                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.10
                     logging:
                       driver: 'json-file'
                       options:
@@ -289,6 +289,7 @@ class OpbeansServiceTest(ServiceTest):
                         - NODE_AGENT_REPO=
                         - OPBEANS_DT_PROBABILITY=0.50
                         - ELASTIC_APM_ENVIRONMENT=production
+                        - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.10
                     depends_on:
                         postgres:
                           condition:
@@ -350,7 +351,6 @@ class OpbeansServiceTest(ServiceTest):
                         - ELASTIC_APM_VERIFY_SERVER_CERT=true
                         - ELASTIC_APM_FLUSH_INTERVAL=5
                         - ELASTIC_APM_TRANSACTION_MAX_SPANS=50
-                        - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.5
                         - ELASTIC_APM_SOURCE_LINES_ERROR_APP_FRAMES
                         - ELASTIC_APM_SOURCE_LINES_SPAN_APP_FRAMES=5
                         - ELASTIC_APM_SOURCE_LINES_ERROR_LIBRARY_FRAMES
@@ -365,6 +365,7 @@ class OpbeansServiceTest(ServiceTest):
                         - PYTHON_AGENT_VERSION
                         - OPBEANS_DT_PROBABILITY=0.50
                         - ELASTIC_APM_ENVIRONMENT=production
+                        - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.10
                     depends_on:
                         postgres:
                           condition:
@@ -453,6 +454,7 @@ class OpbeansServiceTest(ServiceTest):
                       - RUBY_AGENT_VERSION
                       - OPBEANS_DT_PROBABILITY=0.50
                       - ELASTIC_APM_ENVIRONMENT=production
+                      - ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.10
                     logging:
                       driver: 'json-file'
                       options:
@@ -946,6 +948,7 @@ class LocalTest(unittest.TestCase):
                     -E, apm-server.kibana.username=apm_server_user, -E, apm-server.kibana.password=changeme,
                     -E, apm-server.jaeger.http.enabled=true, -E, "apm-server.jaeger.http.host=0.0.0.0:14268",
                     -E, apm-server.jaeger.grpc.enabled=true, -E, "apm-server.jaeger.grpc.host=0.0.0.0:14250",
+                    -E, apm-server.sampling.keep_unsampled=true,
                     -E, 'output.elasticsearch.hosts=["http://elasticsearch:9200"]',
                     -E, output.elasticsearch.username=apm_server_user, -E, output.elasticsearch.password=changeme,
                     -E, output.elasticsearch.enabled=true,
--- scripts/tests/test_service.py
+++ scripts/tests/test_service.py
@@ -403,7 +403,6 @@ class AgentServiceTest(ServiceTest):
                         ELASTIC_APM_API_REQUEST_TIME: '3s'
                         ELASTIC_APM_FLUSH_INTERVAL: '5'
                         ELASTIC_APM_LOG_LEVEL: Info
-                        ELASTIC_APM_TRANSACTION_SAMPLE_RATE: '1'
                         ELASTIC_APM_SERVICE_NAME: dotnetapp
                         ELASTIC_APM_TRANSACTION_IGNORE_NAMES: 'healthcheck'
                         ELASTIC_APM_VERIFY_SERVER_CERT: 'true'
