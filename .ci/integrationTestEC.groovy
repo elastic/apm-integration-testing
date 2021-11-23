88965683200190e6e23943cd44820cc9ee7b8346
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2021-11-23 13:03:01 +0100

    fix: change 7.x to 7.16.0

--- .ci/integrationTestEC.groovy
+++ .ci/integrationTestEC.groovy
@@ -59,7 +59,7 @@ pipeline {
             // The below line is part of the bump release automation
             // if you change anything please modifies the file
             // .ci/bump-stack-release-version.sh
-            values '8.0.0-SNAPSHOT', '7.x', '7.15.2'
+            values '8.0.0-SNAPSHOT', '7.16.0', '7.15.2'
           }
         }
         stages {
