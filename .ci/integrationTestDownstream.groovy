7c0f7a8de87a00897b1ce751c7c342fde3dc0862
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2021-12-02 16:15:02 +0100

    feat: enable pytest_otel plugin (#1331)
    * feat: enable pytest_otel plugin
    
    * Update Makefile

--- .ci/integrationTestDownstream.groovy
+++ .ci/integrationTestDownstream.groovy
@@ -122,7 +122,9 @@ pipeline {
               retryWithSleep(retries: 3, seconds: 5, backoff: true) {
                 sh(label: 'Prepare docker images', script: '.ci/scripts/build-docker-all.sh')
               }
-              sh(label: 'Run all', script: '.ci/scripts/all.sh')
+              withOtelEnv() {
+                sh(label: 'Run all', script: '.ci/scripts/all.sh')
+              }
             }
           }
         }
@@ -226,7 +228,9 @@ def runScript(Map params = [:]){
       sh 'docker ps -a && docker images -a && docker volume ls && docker network ls'
       dir("${BASE_DIR}"){
         withEnv(env){
-          sh(label: "Testing ${agentType}", script: ".ci/scripts/${agentType}.sh")
+          withOtelEnv() {
+            sh(label: "Testing ${agentType}", script: ".ci/scripts/${agentType}.sh")
+          }
           sh 'docker ps -a && docker images -a && docker volume ls && docker network ls'
         }
       }
