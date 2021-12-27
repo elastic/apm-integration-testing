94d46ed53ac9f838db6a0bdfdbfa6fa59f2eee0f
Gil Raphaelli <gil@elastic.co>
2021-12-27 16:15:26 -0500

    Resolve Kibana 8.0 deprecations (#1355)
    * resolve critical kibana deprecations
    * resolve warning kibana deprecations
    
    (cherry picked from commit eb48eb1da81af2355ad589720edc5540be87e0c4)
    
    # Conflicts:
    #	scripts/tests/test_localsetup.py

--- scripts/modules/elastic_stack.py
+++ scripts/modules/elastic_stack.py
@@ -1142,9 +1142,12 @@ class Kibana(StackService, Service):
                 self.environment["XPACK_SECURITY_ENCRYPTIONKEY"] = "fhjskloppd678ehkdfdlliverpoolfcr"
                 self.environment["XPACK_ENCRYPTEDSAVEDOBJECTS_ENCRYPTIONKEY"] = "fhjskloppd678ehkdfdlliverpoolfcr"
             if self.at_least_version("7.8"):
-                self.environment["XPACK_FLEET_AGENTS_ELASTICSEARCH_HOST"] = urls[0]
-                self.environment["XPACK_FLEET_AGENTS_KIBANA_HOST"] = "{}://kibana:{}".format(
-                    "https" if self.kibana_tls else "http", self.SERVICE_PORT)
+                if self.at_least_version("7.16.0"):
+                    self.environment["XPACK_FLEET_AGENTS_ELASTICSEARCH_HOSTS"] = json.dumps(urls)
+                else:
+                    self.environment["XPACK_FLEET_AGENTS_ELASTICSEARCH_HOST"] = urls[0]
+                    self.environment["XPACK_FLEET_AGENTS_KIBANA_HOST"] = "{}://kibana:{}".format(
+                        "https" if self.kibana_tls else "http", self.SERVICE_PORT)
             if options.get("xpack_secure"):
                 self.environment["ELASTICSEARCH_PASSWORD"] = "changeme"
                 self.environment["ELASTICSEARCH_USERNAME"] = "kibana_system_user"
@@ -1157,6 +1160,10 @@ class Kibana(StackService, Service):
             if self.at_least_version("7.6"):
                 if not options.get("no_kibana_apm_servicemaps"):
                     self.environment["XPACK_APM_SERVICEMAPENABLED"] = "true"
+            if self.at_least_version("7.16"):
+                self.environment["XPACK_REPORTING_ROLES_ENABLED"] = "false"
+                self.environment["XPACK_SECURITY_SESSION_IDLETIMEOUT"] = "1M"
+                self.environment["XPACK_SECURITY_SESSION_LIFESPAN"] = "3M"
             if self.kibana_tls:
                 certs = "/usr/share/kibana/config/certs/tls.crt"
                 certsKey = "/usr/share/kibana/config/certs/tls.key"
