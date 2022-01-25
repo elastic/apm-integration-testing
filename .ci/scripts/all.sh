88f9f4704e1dea62e262c8d56dec8ad2da27e00b
Victor Martinez <victormartinezrubio@gmail.com>
2022-01-25 13:39:41 +0000

    Apply suggestions from code review

--- .ci/scripts/all.sh
+++ .ci/scripts/all.sh
@@ -6,34 +6,4 @@ test -z "$srcdir" && srcdir=.
 # shellcheck disable=SC1090
 . "${srcdir}/common.sh"
 
-<<<<<<< HEAD
 prepareAndRunAll env-agent-all docker-test-all
-=======
-# export the variables to force the be defined in the Docker container
-export ELASTIC_APM_SECRET_TOKEN=${ELASTIC_APM_SECRET_TOKEN:-"SuPeRsEcReT"}
-export APM_SERVER_URL=${APM_SERVER_URL:-"https://apm-server:8200"}
-export PYTHONHTTPSVERIFY=0
-
-DEFAULT_COMPOSE_ARGS="${ELASTIC_STACK_VERSION} ${BUILD_OPTS}\
-  --no-apm-server-dashboards \
-  --no-apm-server-self-instrument \
-  --with-agent-rumjs \
-  --with-agent-dotnet \
-  --with-agent-go-net-http \
-  --with-agent-nodejs-express \
-  --with-agent-php-apache \
-  --with-agent-ruby-rails \
-  --with-agent-java-spring \
-  --with-agent-python-django \
-  --with-agent-python-flask \
-  --force-build \
-  --no-xpack-secure \
-  --apm-server-enable-tls \
-  --no-verify-server-cert  \
-  --apm-server-secret-token=${ELASTIC_APM_SECRET_TOKEN} \
-  --apm-server-url=${APM_SERVER_URL} \
-  --apm-log-level=debug"
-
-export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
-runTests env-agent-all docker-test-all
->>>>>>> bc74219 (add basic PHP agent integration tests (#863))
--- .ci/scripts/php.sh
+++ .ci/scripts/php.sh
@@ -3,7 +3,6 @@
 
 srcdir=$(dirname "$0")
 test -z "$srcdir" && srcdir=.
-<<<<<<< HEAD
 # shellcheck disable=SC1090,SC1091
 . "${srcdir}/common.sh"
 
@@ -14,21 +13,16 @@ if [ -n "${APM_AGENT_PHP_VERSION}" ]; then
   BUILD_OPTS="${BUILD_OPTS} ${EXTRA_OPTS}"
 fi
 
-=======
 # shellcheck disable=SC1090
 . "${srcdir}/common.sh"
 
->>>>>>> bc74219 (add basic PHP agent integration tests (#863))
 DEFAULT_COMPOSE_ARGS="${ELASTIC_STACK_VERSION} ${BUILD_OPTS} \
   --no-apm-server-dashboards \
   --no-apm-server-self-instrument \
   --with-agent-php-apache \
   --apm-server-agent-config-poll=1s \
   --force-build \
-<<<<<<< HEAD
   --no-kibana \
-=======
->>>>>>> bc74219 (add basic PHP agent integration tests (#863))
   --no-xpack-secure"
 export COMPOSE_ARGS=${COMPOSE_ARGS:-${DEFAULT_COMPOSE_ARGS}}
 runTests env-agent-php docker-test-agent-php
