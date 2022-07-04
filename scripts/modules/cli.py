807ceaafc8856e472a24ee7138b86e3601b23256
apmmachine <58790750+apmmachine@users.noreply.github.com>
2022-07-04 04:49:25 -0400

    [Automation] Update elastic stack release version to 8.3.1 (#1497)
    Co-authored-by: apmmachine <infra-root-apmmachine@elastic.co>

--- scripts/modules/cli.py
+++ scripts/modules/cli.py
@@ -88,8 +88,10 @@ class LocalSetup(object):
         '8.1': '8.1.3',
         '8.2': '8.2.3',
 
-        'main': '8.2.3',
-        'master': '8.2.3',  # keep master alias for backward compatibility. Upgrade the main alias only
+        '8.3': '8.3.1',
+
+        'main': '8.3.1',
+        'master': '8.3.1',  # keep master alias for backward compatibility. Upgrade the main alias only
     }
 
     def __init__(self, argv=None, services=None):
