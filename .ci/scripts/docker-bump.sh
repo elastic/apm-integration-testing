3b1ee151b07cf85b21fb2eec922693c77fd42af9
Michael Place <mike.place@elastic.co>
2022-08-12 10:55:32 +0200

    Set lang for click install

--- .ci/scripts/docker-bump.sh
+++ .ci/scripts/docker-bump.sh
@@ -6,5 +6,7 @@ srcdir=$(dirname "${0}")
 test -z "$srcdir" && srcdir=.
 # shellcheck disable=SC1090
 . "${srcdir}/common.sh"
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 
 prepareAndRunGoals test-docker-bump
