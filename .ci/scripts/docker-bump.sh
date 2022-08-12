80ac8d53f27440b64d1738d6597b06f2656606b0
Michael Place <mike.place@elastic.co>
2022-08-12 16:42:49 +0200

    make linter happy

--- .ci/scripts/docker-bump.sh
+++ .ci/scripts/docker-bump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # for details about how it works see https://github.com/elastic/apm-integration-testing#continuous-integration
 
-REUSE_CONTAINERS=1
+export REUSE_CONTAINERS=1
 srcdir=$(dirname "${0}")
 test -z "$srcdir" && srcdir=.
 # shellcheck disable=SC1090
