0f7de0ea949b9a205275309d439ece92d34fca67
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-08-26 17:11:11 +0100

    [Automation] Update elastic stack release version to 8.5.0

--- scripts/modules/cli.py
+++ scripts/modules/cli.py
@@ -90,8 +90,9 @@ class LocalSetup(object):
 
         '8.3': '8.3.3',
 
-        'main': '8.3.3',
-        'master': '8.3.3',  # keep master alias for backward compatibility. Upgrade the main alias only
+        '8.5': '8.5.0',
+        'main': '8.5.0',
+        'master': '8.5.0',  # keep master alias for backward compatibility. Upgrade the main alias only
     }
 
     def __init__(self, argv=None, services=None):
