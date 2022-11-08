9380b411ae82c423cb844cdcd2b899a778079c6c
Victor Martinez <VictorMartinezRubio@gmail.com>
2022-11-08 09:44:20 +0000

    fix linting shell

--- scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs/scripts/setup.sh
+++ scripts/kibana/validate-ts-interfaces-against-apm-server-sample-docs/scripts/setup.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 rm -rf tmp
 mkdir tmp
 mkdir tmp/apm-server-docs
 
 # download apm server samples
-yarn ts-node ./scripts/download-sample-docs.ts $1 $2
+yarn ts-node ./scripts/download-sample-docs.ts "$1" "$2"
 
 # Clone kibana and copy ts interfaces
-./scripts/clone-kibana.sh $3 $4
+./scripts/clone-kibana.sh "$3" "$4"
