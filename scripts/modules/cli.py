6441f4a6c6b04cc0911df97ecbce1a0e9017b686
apmmachine <apmmachine@users.noreply.github.com>
2023-02-15 15:00:37 +0000

    chore: Updated the content of the file "/tmp/updatecli/github/elastic...
    ... /apm-integration-testing/scripts/modules/cli.py"
    
    Made with ❤️️ by updatecli

--- scripts/modules/cli.py
+++ scripts/modules/cli.py
@@ -89,8 +89,8 @@ class LocalSetup(object):
         '8.6': '8.6.1',
         '8.7': '8.7.0',
         # UPDATECLI_AUTOMATION
-        "main": "8.7.0",
-        "master": "8.7.0",  # keep master alias for backward compatibility. Upgrade the main alias only
+        "main": "8.8.0",
+        "master": "8.8.0",  # keep master alias for backward compatibility. Upgrade the main alias only
     }
 
     def __init__(self, argv=None, services=None):
