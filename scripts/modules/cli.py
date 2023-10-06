8cdab21003b7a16a6dd4da16e77c9d0bb5f353ce
apmmachine <58790750+apmmachine@users.noreply.github.com>
2023-10-06 04:30:09 -0400

    chore: Updated to content "" in file "scripts/modules/cli.py" (#1574)
    Made with ❤️️ by updatecli
    
    Co-authored-by: apmmachine <apmmachine@users.noreply.github.com>

--- scripts/modules/cli.py
+++ scripts/modules/cli.py
@@ -92,8 +92,8 @@ class LocalSetup(object):
         '8.9': '8.9.0',
         '8.10': '8.10.0',
         # UPDATECLI_AUTOMATION
-        "main": "8.10.0",
-        "master": "8.10.0",  # keep master alias for backward compatibility. Upgrade the main alias only
+        "main": "8.11.0",
+        "master": "8.11.0",  # keep master alias for backward compatibility. Upgrade the main alias only
     }
 
     def __init__(self, argv=None, services=None):
